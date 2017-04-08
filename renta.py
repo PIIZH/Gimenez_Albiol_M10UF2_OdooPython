6e4d5091a96191842f0018eae9938047f49fdc35
PIIZH <dam2@debm06ex.fjeclot.net>
2017-04-09 01:23:20 +0200

    finished

--- renta.py
+++ renta.py
@@ -5,31 +5,57 @@ from openerp import models, fields, api
 class renta(models.Model):
      _name = 'renta.renta'
      _salaryB = 'renta.salary'
-
-     name = fields.Char(string = "DNI de la persona que realitza la renta: ", required = True) # DNI - String
-     salaryB = fields.Float(string = "Salari brut mensual: ", required = True) # salari brut mensual - Float
-     valueIM = fields.Float(string = "Valor immobiliari: ", required = True) # valor immobiliari
-     valueMB = fields.Float(string = "Valor mobiliari: ", required = True) # valor mobiliari
+   
+     name = fields.Char(string = "DNI de la persona que realitza la renta: ", required = True) 
+     salaryB = fields.Float(string = "Salari brut mensual: ", required = True) 
+          
+     children = fields.Integer(string = "Número de fills ", required = False) 
+     parent = fields.Boolean(string = "Tens alguna ancia residint en el teu domicili? ", default = False)
      
-     stateAlone = fields.Boolean(string = "Solter/a ", required = False, default = True)
-     stateMarried = fields.Boolean(string = "Casat/ada ", required = False)
-     stateDead = fields.Boolean(string = "Vidu/a ", required = False)
+     childrenDown = fields.Boolean(string = "Tens fills amb discapacitats? ", default = False) 
      
-     children = fields.Integer(string = "Fills menors a 3 anys: ", required = False) # fills < 3 - Integer
-     childrenOld = fields.Integer(string = "Fills igual o majors a 3 anys: ", required = False) # fills >= 3 - Integer
-     parent = fields.Integer(string = "Pares majors de 56 ", default = False) # pares > 56 - Integer
-     parentOld = fields.Integer(string = "Pares majors de 76 ", default = False) # pares > 76 - Integer
-     
-     childrenDown = fields.Boolean(string = "Tens fills amb discapacitats? ", default = False) # fills amb discapacitat - Boolean
-     
-     result = fields.Float(string = "Resultat del cálcul: ", default = 0, readonly = True) # resultat dels calculs
+     result = fields.Float(string = "Resultat del cálcul: ", default = 0, readonly = True) 
      
      @api.one
-     def calcSalaryAnual(self):
-		 self.result = self.salaryB * 14
-		 #self.foo() # peta
-
-
-	 #def foo(self):
-	 #	 self.stateMarried = True
+     def calcRenta(self):
+		 _percentNorm = 1 
+		 _cashFills = 0
+		 _disca = 0
+		 if len(self.name)!=9:
+			self.name = "DNI mal introduit"
+			 
+		 elif (self.salaryB *14)> 0 and (self.salaryB *14)<12451:
+			_percentNorm = 19.5
+		 
+		 elif (self.salaryB *14)> 12451 and (self.salaryB *14) < 20200:
+			_percentNorm = 24.5
+		 
+		 elif(self.salaryB *14) > 20201 and(self.salaryB *14) < 35200:
+			_percentNorm = 30.5
+		 
+		 elif (self.salaryB *14) > 35201 and (self.salaryB *14) < 60000:
+			_percentNorm = 38
+		 
+		 elif (self.salaryB *14) > 60001:
+			_percentNorm = 46
+		 
+		 if self.children >= 5:
+			_cashFills = 5*150
+		 
+		 else:
+			_cashFills = self.children*150
+		 
+		 if self.parent and (self.salaryB*14)<24020:
+			 _percentNorm -= 10.05
+		
+		 if self.childrenDown and (self.salaryB*14)>=30000:
+			_disca = 300
+		 
+		 else: 
+			_percentNorm -= 10
+		 	
+		 self.result = format(((self.salaryB *14)-(_percentNorm*(self.salaryB *14))/100)+_cashFills+_disca,'.0f') 
+		
+					
+					
 		 
